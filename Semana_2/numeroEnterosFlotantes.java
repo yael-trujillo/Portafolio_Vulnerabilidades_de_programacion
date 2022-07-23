c8267cd5ff8d36e7d32dcb9d44ff97ac37571c16
yael-trujillo <len_yiyan03@outlook.com>
2022-07-23 11:30:04 -0500

    archivo_java

---
+++ Semana_2/numeroEnterosFlotantes.java
@@ -0,0 +1,44 @@
+
+public class numeroEnterosFlotantes {
+
+    public static void main(String args[]) {
+        /*
+            Tipos primitivos enteros
+            Byte = 8 bits
+            short = 16 bits
+            int = 32 bits
+            long = 64 bits
+        
+            Almacenan valores positivos y negativos
+         */
+
+        System.out.println("##########NUMEROS ENTEROS#########");
+        System.out.println("Valor minimo de byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor maximo de byte: " + Byte.MAX_VALUE);
+
+        System.out.println("\nValor minimo de short: " + Short.MIN_VALUE);
+        System.out.println("Valor maximo de short: " + Short.MAX_VALUE);
+
+        System.out.println("\nValor minimo de int: " + Integer.MIN_VALUE);
+        System.out.println("Valor maximo de int: " + Integer.MAX_VALUE);
+
+        System.out.println("\nValor minimo de long: " + Long.MIN_VALUE);
+        System.out.println("Valor maximo de long: " + Long.MAX_VALUE);
+
+        /*
+            Tipos primitivos flotantes
+            float = 32 bits
+            double = 64 bits
+        
+            Almacenan valores positivos y negativos
+         */
+        
+        System.out.println("\n##########NUMEROS DECIMALES##########");
+
+        System.out.println("\nValor minimo de float: " + Float.MIN_VALUE);
+        System.out.println("Valor maximo de float: " + Float.MAX_VALUE);
+
+        System.out.println("\nValor minimo de double: " + Double.MIN_VALUE);
+        System.out.println("Valor maximo de double: " + Double.MAX_VALUE);
+    }
+}
